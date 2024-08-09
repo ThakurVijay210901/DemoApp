59c509640bb9fc036105fda5553580c397e7a210
Vijay Thakur <vijay@bcoder.co.in>
2024-08-09 13:02:17 +0530

    Script Code Updated

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,9 +1,15 @@
-#!/bin/sh
+#!/bin/zsh
 
+# Ensure .netrc file is present
 touch ~/.netrc
 
+# Set up Mapbox credentials in .netrc
 echo "machine api.mapbox.com" > ~/.netrc
 echo "login mapbox" >> ~/.netrc
 echo "password ${MAPBOX_TOKEN}" >> ~/.netrc
+
+# Install CocoaPods and run pod install
 brew install cocoapods
 pod install
+
+chmod +x ci_post_clone.sh
